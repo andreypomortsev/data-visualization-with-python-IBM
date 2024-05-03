6f5c1f04e416bc1184adf79761281ba24ac49778
Andrey <andreypomortsev@gmail.com>
2024-05-03 20:25:45 +0500

    Created a few of the grafs for Recession Report Statistics

--- DV0101EN-Final-Assign-Part-2-Questions.py
+++ DV0101EN-Final-Assign-Part-2-Questions.py
@@ -59,8 +59,8 @@ app.layout = html.Div([
     Output(component_id='select year', component_property='disabled'),
     Input(component_id='dropdown-statistics',component_property='value'))
 
-def update_input_container(value):
-    return value != 'Yearly Statistics'
+def update_input_container(statistics: str) -> bool:
+    return statistics != 'Yearly Statistics'
 
 #Callback for plotting
 # Define the callback function to update the input container based on the selected statistics
@@ -69,8 +69,8 @@ def update_input_container(value):
     [Input(component_id='select-year', component_property='value'), Input(component_id='dropdown-statistics', component_property='value')])
 
 
-def update_output_container(selected_year, selected_statistic):
-    if selected_statistic == 'Recession Period Statistics':
+def update_output_container(input_year, selected_statistics):
+    if selected_statistics == 'Recession Period Statistics':
         # Filter the data for recession periods
         recession_data = data[data['Recession'] == 1]
         
@@ -78,59 +78,66 @@ def update_output_container(selected_year, selected_statistic):
 
 #Plot 1 Automobile sales fluctuate over Recession Period (year wise)
         # use groupby to create relevant data for plotting
-        yearly_rec=recession_data.groupby('...')['...'].mean().reset_index()
+        yearly_rec = recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
         R_chart1 = dcc.Graph(
-            figure=px.......(".....", 
-                x='....',
-                y='......',
+            figure=px.line(yearly_rec, 
+                x='Year',
+                y='Average Automobile Sales',
                 title="Average Automobile Sales fluctuation over Recession Period"))
 
 #Plot 2 Calculate the average number of vehicles sold by vehicle type       
         # use groupby to create relevant data for plotting
-        average_sales = ...............mean().reset_index()                           
-        R_chart2  = dcc.Graph(figure=px....................
+        average_sales = recession_data.groupby("Vehicle_Type")['Automobile_Sales'].mean().reset_index()                           
+        R_chart2  = dcc.Graph(figure=px.pie(average_sales))
         
 # Plot 3 Pie chart for total expenditure share by vehicle type during recessions
         # use groupby to create relevant data for plotting
-        exp_rec= ....................
-        R_chart3 = .............
+        exp_rec= recession_data.groupby("Vehicle_Type")['Advertising_Expenditure'].mean().reset_index()
+        R_chart3 = dcc.Graph(
+                figure=px.pie(exp_rec,
+                values='Advertising_Expenditure',
+                names='Vehicle_Type',
+                title="Total Expenditure Share by Vehicle Type"
+                )
+        )
 
 # Plot 4 bar chart for the effect of unemployment rate on vehicle type and sales
-        ................
-        ...................
+        unempl_vt_sales = recession_data.groupby("Vehicle_Type")['Automobile_Sales'].mean().reset_index()
+        R_chart4 = dcc.Graph(figure=px.bar(unempl_vt_sales, x="Vehicle_Type", y="Automobile_Sales"))
 
 
         return [
-            html.Div(className='..........', children=[html.Div(children=R_chart1),html.Div(children=.....)],style={.....}),
-            html.Div(className='chart-item', children=[html.Div(children=...........),html.Div(.............)],style={....})
+            html.Div(className='chart-item', children=[html.Div(children=R_chart1),html.Div(children=R_chart2)],style={.....}),
+            html.Div(className='chart-item', children=[html.Div(children=R_chart3),html.Div(children=R_chart4)],style={....})
             ]
 
 # TASK 2.6: Create and display graphs for Yearly Report Statistics
  # Yearly Statistic Report Plots                             
-    elif (input_year and selected_statistics=='...............') :
-        yearly_data = data[data['Year'] == ......]
+    elif (input_year and selected_statistics=='Yearly Statistics') :
+        yearly_data = data[data['Year'] == input_year]
                               
 #TASK 2.5: Creating Graphs Yearly data
                               
 #plot 1 Yearly Automobile sales using line chart for the whole period.
         yas= data.groupby('Year')['Automobile_Sales'].mean().reset_index()
-        Y_chart1 = dcc.Graph(figure=px.line(.................))
+        Y_chart1 = dcc.Graph(figure=px.line(yas))
             
 # Plot 2 Total Monthly Automobile sales using line chart.
-        Y_chart2 = dcc.Graph(................)
+        mas = data.groupby('Month')['Automobile_Sales'].mean().reset_index()
+        Y_chart2 = dcc.Graph(figure=px.line(mas))
 
             # Plot bar chart for average number of vehicles sold during the given year
-        avr_vdata=yearly_data.groupby........................
-        Y_chart3 = dcc.Graph( figure.................,title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year)))
+        avr_vdata=yearly_data.groupby()
+        Y_chart3 = dcc.Graph(figure=px.bar(avr_vdata),title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year))
 
             # Total Advertisement Expenditure for each vehicle using pie chart
-        exp_data=yearly_data.groupby(..................
-        Y_chart4 = dcc.Graph(...............)
+        exp_data=yearly_data.groupby("Vehicle_Type")["Advertisement_Expenditure"].sum().reset_index()
+        Y_chart4 = dcc.Graph(figure=px.pie(exp_data))
 
 #TASK 2.6: Returning the graphs for displaying Yearly data
         return [
-                html.Div(className='.........', children=[html.Div(....,html.Div(....)],style={...}),
-                html.Div(className='.........', children=[html.Div(....),html.Div(....)],style={...})
+                html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)],style={...}),
+                html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)],style={...})
                 ]
         
     else:
